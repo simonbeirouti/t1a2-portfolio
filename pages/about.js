a7bf493ba0c3d92d3840b66cbf8ad13efcfbba86
simonbeirouti <hello@simonbeirouti.com>
2022-06-08 23:46:02 +1000

    create about page with hero component

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Hero from '../components/Hero';
+
+export default function AboutPage() {
+    return (
+        <Hero 
+            description="Learn a bit more about me"
+        />
+    );
+}
