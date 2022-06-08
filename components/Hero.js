a4f1a643803a3ac8590bf1871b526e353f4ce53b
simonbeirouti <hello@simonbeirouti.com>
2022-06-08 23:45:36 +1000

    create hero component for multiple pages

---
+++ components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Hero({description}) {
+    return (
+        <div className="hero-section">
+            <h1>{description}</h1>
+        </div>
+    )
+}
