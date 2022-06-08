a1039d0de67184cff02e3439b2f12b6230994907
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 00:22:17 +1000

    add styling to component for tech used

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -1,12 +1,14 @@
 import Image from "next/image"
 
-export default function ProjectPost({name, description, image}) {
+export default function ProjectPost({name, description, image, technologies}) {
     return (
         <>
             <Image height={300} width={400} src={image} alt={description} />
             <div className="project-text">
                 <h1>{name}</h1>
                 <p>{description}</p>
+                <br/>
+                <p className="tech-used">{technologies}</p>
             </div>
         </>
     )
