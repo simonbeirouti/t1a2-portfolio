91e75d921ae79a03b8df00bb4d419fe7fffdbbf7
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 23:56:42 +1000

    fix key issue and add home button

--- components/Header.js
+++ components/Header.js
@@ -2,30 +2,31 @@ import React from 'react';
 import Link from 'next/link';
 
 const navItems = [
-  {link: '/', icon: '🏠'},
-  {link: 'about', icon: '📖'},
-  {link: 'projects', icon: '🛠'},
-  {link: 'blog', icon: '✍🏼'},
-]
+  {no: 1, link: '/about', icon: '📖'},
+  {no: 2, link: '/projects', icon: '🛠'},
+  {no: 3, link: '/blog', icon: '✍🏼'},
+];
 
 export default function Header() {
   return (
     <header>
-        <div className="logo i-hate-css">
-            <p>PORTFOLIO</p>
-        </div>
-          <nav>
-              <ul id="nav-links">
-                  {navItems.map((item) => {
-                    return (
-                      <li>
-                        <Link href={item.link}><a><button className="btn">{item.icon}</button></a></Link>
-                      </li>  
-                    )
-                  })}
-              </ul>
-          </nav>
-        <Link href="contact"><a><button className="btn cta">📲</button></a></Link>
+        <Link href="/"><a><button className="btn home">🏠</button></a></Link>
+        <nav>
+            <ul id="nav-links">
+                {navItems.map((item) => {
+                  return (
+                    <li key={item.no}>
+                      <Link href={item.link}>
+                        <a>
+                          <button className="btn">{item.icon}</button>
+                        </a>
+                      </Link>
+                    </li>  
+                  )
+                })}
+            </ul>
+        </nav>
+        <Link href="/contact"><a><button className="btn cta">📲</button></a></Link>
     </header>
   )
 }
