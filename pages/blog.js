8679057128219796a09b73997a2723e2cb7a9f11
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 23:57:52 +1000

    create blog page and then made it more dynamic

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,24 @@
 import MetaHead from '../components/MetaHead';
 import Hero from '../components/Hero';
+import BlogPost from '../components/BlogPost';
+
+const blogPosts = [
+    {
+        name: "Portfolio project",
+        excerpt: "Est succulents blue bottle authentic ea swag bushwick man braid",
+        link: "#"
+    },
+    {
+        name: "Budget app",
+        excerpt: "Est succulents blue bottle authentic ea swag bushwick man braid",
+        link: "#"
+    },
+    {
+        name: "News scraper",
+        excerpt: "Est succulents blue bottle authentic ea swag bushwick man braid",
+        link: "#"
+    }
+];
 
 export default function BlogPage() {
     return (
@@ -9,8 +28,19 @@ export default function BlogPage() {
                 description="Blog page to read and watch about the different things that Simon is up to."
             />
             <Hero 
-                description="Content that I create"
+                description="Blog"
             />
+            <section className='blog-parent'>
+                {blogPosts.map((post) => {
+                    return (
+                        <BlogPost 
+                            name={post.name}
+                            excerpt={post.excerpt}
+                            link={post.link}
+                        />
+                    )
+                })}
+            </section>
         </>
     );
 }
