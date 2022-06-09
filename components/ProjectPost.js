d104146a6b8dd1ee611428c823cd01d4a1b9980a
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 23:57:02 +1000

    fix the project post button to be more dynamic

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -1,14 +1,19 @@
 import Image from "next/image"
 
-export default function ProjectPost({name, description, image, technologies}) {
+export default function ProjectPost({name, description, image, technologies, link}) {
     return (
         <>
-            <Image height={300} width={400} src={image} alt={description} />
-            <div className="project-text">
-                <h1>{name}</h1>
-                <p>{description}</p>
-                <br/>
-                <p className="tech-used">{technologies}</p>
+            <div className="project">
+                <Image height={300} width={400} src={image} alt={description} />
+                <div className="project-text">
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                    <br/>
+                    <p className="tech-used">{technologies}</p>
+                </div>
+                <a href={link}>
+                    <button>Go to project</button>
+                </a>
             </div>
         </>
     )
