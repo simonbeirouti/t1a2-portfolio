dc46a37a2f1e643aa1047bbdde8a63dfc266edda
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 23:57:36 +1000

    give unique keys

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,21 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const hobbies = [
-  {hobbieName: 'Exercising', link: 'https://www.risenation.com.au/'},
-  {hobbieName: 'Learning', link: 'https://www.coderacademy.edu.au/'},
-  {hobbieName: 'Travelling', link: 'https://en.wikipedia.org/wiki/Globe'},
-  {hobbieName: 'Reading', link: 'https://www.booktopia.com.au/principles-ray-dalio/book/9781501124020.html'}
+  {no: 1, hobbieName: 'Exercising', link: 'https://www.risenation.com.au/'},
+  {no: 2, hobbieName: 'Learning', link: 'https://www.coderacademy.edu.au/'},
+  {no: 3, hobbieName: 'Travelling', link: 'https://en.wikipedia.org/wiki/Globe'},
+  {no: 4, hobbieName: 'Reading', link: 'https://www.booktopia.com.au/principles-ray-dalio/book/9781501124020.html'}
 ];
 const technologies =[
-  {techName: 'Oura Ring', link: 'https://ouraring.com/'}, 
-  {techName: 'MacBook Pro', link: 'https://www.apple.com/au/macbook-pro-14-and-16/'}, 
-  {techName: 'iPhone', link: 'https://www.apple.com/au/iphone-12/key-features/'},
-  {techName: 'Infrared Saunas', link: 'https://infraredi.com.au/products/infraredi-mid'}
+  {no: 1, techName: 'Oura Ring', link: 'https://ouraring.com/'}, 
+  {no: 2, techName: 'MacBook Pro', link: 'https://www.apple.com/au/macbook-pro-14-and-16/'}, 
+  {no: 3, techName: 'iPhone', link: 'https://www.apple.com/au/iphone-12/key-features/'},
+  {no: 4, techName: 'Infrared Saunas', link: 'https://infraredi.com.au/products/infraredi-mid'}
 ];
 const foods = [
-  {foodName: 'Organic Meat', link: 'https://hagensorganics.com.au/'}, 
-  {foodName: 'Blue Dinosaur', link: 'https://bluedinosaur.com.au/'}, 
-  {foodName: 'Life Cykel', link: 'https://lifecykel.com/'} 
+  {no: 1, foodName: 'Organic Meat', link: 'https://hagensorganics.com.au/'}, 
+  {no: 2, foodName: 'Blue Dinosaur', link: 'https://bluedinosaur.com.au/'}, 
+  {no: 3, foodName: 'Life Cykel', link: 'https://lifecykel.com/'} 
 ];
 
 export default function Home() {
@@ -40,7 +40,7 @@ export default function Home() {
           <ul className='liked-ulist'>
             {hobbies.map((hobby) => {
               return (
-                  <Link href={hobby.link}>
+                  <Link key={hobby.no} href={hobby.link}>
                     <li>{hobby.hobbieName}</li> 
                   </Link>
               )
@@ -52,7 +52,7 @@ export default function Home() {
           <ul className='liked-ulist'>
             {technologies.map((thing) => {
               return (
-                <Link href={thing.link}>
+                <Link key={thing.no} href={thing.link}>
                   <li>{thing.techName}</li> 
                 </Link>
               )
@@ -64,7 +64,7 @@ export default function Home() {
           <ul className='liked-ulist'>
             {foods.map((food) => {
               return (
-                <Link href={food.link}>
+                <Link key={food.no} href={food.link}>
                   <li>{food.foodName}</li>
                 </Link>
               )
