0370191e4b33437e2f111a2927924e6bf85a3628
simonbeirouti <hello@simonbeirouti.com>
2022-06-09 23:57:15 +1000

    make the project listing more dynamic

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,30 @@ import MetaHead from "../components/MetaHead";
 import Hero from "../components/Hero";
 import ProjectPost from "../components/ProjectPost";
 
+const projectPosts = [
+    {
+        name: "Portfolio creation",
+        description: "Building a website from the ground up using HTML and CSS.",
+        technologies: "NextJS, HTML, CSS, React",
+        image: "/assets/code.jpg",
+        link: "https://github.com/simonbeirouti/t1a2-portfolio"
+    },
+    {
+        name: "Application development",
+        description: "Creating a working prototype of ideas that operate on phones.",
+        technologies: "Dart, Flutter, Firebase",
+        image: "/assets/ux.jpg",
+        link: "https://github.com/simonbeirouti/flutter_budget_tracker"
+    },
+    {
+        name: "Become h4ck3R",
+        description: "Play around with Node to scrap news data on the war in Ukraine",
+        technologies: "NodeJS, Javascript, Coffee",
+        image: "/assets/hacker.jpg",
+        link: "https://github.com/simonbeirouti/news-scrapper"
+    }
+];
+
 export default function ProjectPage() {
     return (
     <>
@@ -13,30 +37,17 @@ export default function ProjectPage() {
             description="Projects"
         />
         <div className="project-parent">
-            <div className="project">
-                <ProjectPost 
-                    name="Portfolio creation"
-                    description="Building a website from the ground up using HTML and CSS."
-                    technologies="NextJS, HTML, CSS, React"
-                    image="/assets/code.jpg"
-                />
-            </div>
-            <div className="project">
-                <ProjectPost 
-                    name="Application development"
-                    description="Creating a working prototype of ideas that operate on phones."
-                    technologies="Dart, Flutter, Firebase"
-                    image="/assets/ux.jpg"
-                />
-            </div>
-            <div className="project">
-                <ProjectPost 
-                    name="Become h4ck3R"
-                    description="Write python scipts to order pizza and coffee from local biznesses."
-                    technologies="BASH, Python, Coffee"
-                    image="/assets/hacker.jpg"
-                />
-            </div>
+            {projectPosts.map((post) => {
+                return (
+                    <ProjectPost 
+                        name={post.name}
+                        description={post.description}
+                        technologies={post.technologies}
+                        image={post.image}
+                        link={post.link}
+                    />
+                )
+            })}
         </div>
     </>
     )
