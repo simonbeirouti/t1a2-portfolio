347f70db5e0094a28f78c3c5f6d77b8ebe16911d
simonbeirouti <hello@simonbeirouti.com>
2022-06-11 23:29:17 +1000

    add classes to clean up css

--- components/Header.js
+++ components/Header.js
@@ -2,25 +2,29 @@ import React from 'react';
 import Link from 'next/link';
 
 const navItems = [
-  {no: 1, link: '/about', icon: '📖'},
-  {no: 2, link: '/projects', icon: '🛠'},
+  {no: 1, link: '/about', icon: '❓'},
+  {no: 2, link: '/projects', icon: '👷🏼'},
   {no: 3, link: '/blog', icon: '✍🏼'},
 ];
 
 export default function Header() {
   return (
-    <header>
+    <header className="sm-pt sm-pb sm-pl sm-pr">
         <div>
-          <Link href="/"><a><button className="btn home">🏠</button></a></Link>
+          <Link href="/">
+            <a>
+              <button id="home" className="dashed transition xs-mt xs-mb">サイモン</button>
+            </a>
+          </Link>
         </div>
-        <nav>
+        <nav className='sm-pt sm-pb sm-pl sm-pr'>
             <ul id="nav-links">
                 {navItems.map((item) => {
                   return (
-                    <li key={item.no}>
+                    <li className='sm-pl sm-pr' key={item.no}>
                       <Link href={item.link}>
-                        <a>
-                          <button className="btn">{item.icon}</button>
+                        <a className="transition">
+                          <button id="std" className="dashed transition lg-f xs-mt xs-mb">{item.icon}</button>
                         </a>
                       </Link>
                     </li>  
@@ -29,7 +33,11 @@ export default function Header() {
             </ul>
         </nav>
         <div>
-          <Link href="/contact"><a><button className="btn cta">📲</button></a></Link>
+          <Link href="/contact">
+            <a>
+              <button id="cta" className="dashed transition xs-mt xs-mb">📲</button>
+            </a>
+          </Link>
         </div>
     </header>
   )
