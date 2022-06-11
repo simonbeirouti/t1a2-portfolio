ccc8e4b779582f97f3b45e3e27da7491ed59d06b
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 00:02:42 +1000

    add classes

--- pages/contact.js
+++ pages/contact.js
@@ -13,11 +13,11 @@ export default function ContactPage() {
             />
             {/* Apply spacing to overall component */}
             <form className="xxl-mt xxl-mb" action="#" method="POST">
-                <input className="form-fields dashed" placeholder="First name" type="text" id="first-name" name="first-name" />
+                <input className="form-fields sm-pl sm-mb dashed" placeholder="First name" type="text" id="first-name" name="first-name" />
                 <input type="hidden" name="form-name" value="contact" />
-                <input className="form-fields dashed" placeholder="Last name" type="text" id="last-name" name="last-name" />
-                <input className="form-fields dashed" placeholder="Email address" type="text" id="email-adress" name="email-adress" />
-                <textarea className="textarea dashed" placeholder="Comments? Suggestions?" type="textarea" id="comments-suggestions" name="comments-suggestions" />
+                <input className="form-fields sm-pl dashed" placeholder="Last name" type="text" id="last-name" name="last-name" />
+                <input className="form-fields sm-pl dashed" placeholder="Email address" type="text" id="email-adress" name="email-adress" />
+                <textarea className="textarea sm-pt sm-pl dashed" placeholder="Comments? Suggestions?" type="textarea" id="comments-suggestions" name="comments-suggestions" />
                 <button id="cta" className="dashed transition" type="submit">Submit</button>
             </form>
         </>
