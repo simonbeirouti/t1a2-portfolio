4d1794c648be0e474c30a2bd78216628b5f0298f
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 06:18:42 +1000

    add name and email in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <footer className="xxl-mt">
-            <p>Made with ☕️ and 🍕</p>
+            <p>Simon Beirouti |<Link href="mailto:hello@simonbeirouti.com"><a>hello@simonbeirouti.com</a></Link></p>
         </footer>
     )
 }
