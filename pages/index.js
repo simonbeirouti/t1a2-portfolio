390cefbf23eb0ba9efc0883c2aba753e127e4280
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 01:17:55 +1000

    add more styling to page and clean up structure

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,14 @@ const hobbies = [
   {no: 3, hobbieName: 'Travelling', link: 'https://en.wikipedia.org/wiki/Globe'},
   {no: 4, hobbieName: 'Reading', link: 'https://www.booktopia.com.au/principles-ray-dalio/book/9781501124020.html'}
 ];
-const technologies =[
+
+const technologies = [
   {no: 1, techName: 'Oura Ring', link: 'https://ouraring.com/'}, 
   {no: 2, techName: 'MacBook Pro', link: 'https://www.apple.com/au/macbook-pro-14-and-16/'}, 
   {no: 3, techName: 'iPhone', link: 'https://www.apple.com/au/iphone-12/key-features/'},
   {no: 4, techName: 'Infrared Saunas', link: 'https://infraredi.com.au/products/infraredi-mid'}
 ];
+
 const foods = [
   {no: 1, foodName: 'Organic Meat', link: 'https://hagensorganics.com.au/'}, 
   {no: 2, foodName: 'Blue Dinosaur', link: 'https://bluedinosaur.com.au/'}, 
@@ -29,43 +31,43 @@ export default function Home() {
         description="The home page to find some basic information about Simon"
       />
       <Hero 
-        description="Hi! I am Simon"
+        description="Hi!"
       />
-      <div className='avatar-space'>
-        <Image className='avatar' width={200} height={200} src='/assets/avatar.png' alt='Image of Simon'/>
+      <div className="avatar-space xl-mt">
+        <Image className="avatar" width={200} height={200} src='/assets/avatar.png' alt='Image of Simon'/>
       </div>
-      <div className='holder'>
+      <div className="holder xxl-mt xxl-mb">
         <div>
-          <h1 className='description'>HOBBIES</h1>
-          <ul className='liked-ulist'>
+          <h1 className="description lg-mt">HOBBIES</h1>
+          <ul className="liked-ulist">
             {hobbies.map((hobby) => {
               return (
                   <Link key={hobby.no} href={hobby.link}>
-                    <li>{hobby.hobbieName}</li> 
+                    <li className="favs sm-m dashed sm-pt sm-pb md-pl md-pr">{hobby.hobbieName}</li> 
                   </Link>
               )
             })}
           </ul>
         </div>
         <div>
-          <h1 className='description'>TECHNOLOGIES</h1>
-          <ul className='liked-ulist'>
+          <h1 className="description lg-mt">TECHNOLOGIES</h1>
+          <ul className="liked-ulist">
             {technologies.map((thing) => {
               return (
                 <Link key={thing.no} href={thing.link}>
-                  <li>{thing.techName}</li> 
+                  <li className="favs sm-m dashed sm-pt sm-pb md-pl md-pr">{thing.techName}</li> 
                 </Link>
               )
             })}
           </ul>
         </div>
         <div>
-          <h1 className='description'>FOODS</h1>
-          <ul className='liked-ulist'>
+          <h1 className="description lg-mt">FOODS</h1>
+          <ul className="liked-ulist">
             {foods.map((food) => {
               return (
                 <Link key={food.no} href={food.link}>
-                  <li>{food.foodName}</li>
+                  <li className="favs sm-m dashed sm-pt sm-pb md-pl md-pr">{food.foodName}</li>
                 </Link>
               )
             })}
