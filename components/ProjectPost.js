01d56359df661bdb0620bef42a996628c1742af5
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 01:16:33 +1000

    add styling into components

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -3,16 +3,15 @@ import Image from "next/image"
 export default function ProjectPost({name, description, image, technologies, link}) {
     return (
         <>
-            <div className="project">
+            <div className="project xxl-m">
                 <Image height={300} width={400} src={image} alt={description} />
-                <div className="project-text">
-                    <h1>{name}</h1>
-                    <p>{description}</p>
-                    <br/>
-                    <p className="tech-used">{technologies}</p>
+                <div className="project-text sm-mt sm-mb xxl-ml xxl-mr">
+                    <h1 className="lg-f lg-fw">{name}</h1>
+                    <p className="md-f md-fw">{description}</p>
+                    <p className="tech-used md-f md-fw lg-mt lg-mb">{technologies}</p>
                 </div>
                 <a href={link}>
-                    <button className="cta">Go to project</button>
+                    <button id="cta" className="dashed transition">Go to project</button>
                 </a>
             </div>
         </>
