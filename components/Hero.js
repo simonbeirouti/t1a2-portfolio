aaa3502a0cbc3c90dc7ed8f812ecb933b0c2f791
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 00:02:23 +1000

    increase font size and weight

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Hero({description}) {
     return (
         <div className="hero-section xxl-pt">
-            <h1>{description}</h1>
+            <h1 className="xl-f xl-fw">{description}</h1>
         </div>
     )
 }
