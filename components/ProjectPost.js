fb20c7ee31c9999eff0674040ed2cbe34663d1ab
simonbeirouti <hello@simonbeirouti.com>
2022-06-12 20:27:10 +1000

    remove jsx fragment

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -2,18 +2,16 @@ import Image from "next/image"
 
 export default function ProjectPost({name, description, image, technologies, link}) {
     return (
-        <>
-            <div className="project xxl-m">
-                <Image height={300} width={400} src={image} alt={description} />
-                <div className="project-text sm-mt sm-mb xxl-ml xxl-mr">
-                    <h1 className="lg-f lg-fw">{name}</h1>
-                    <p className="md-f md-fw">{description}</p>
-                    <p className="tech-used md-f md-fw lg-mt lg-mb">{technologies}</p>
-                </div>
-                <a href={link}>
-                    <button id="cta" className="dashed transition">Go to project</button>
-                </a>
+        <div className="project xxl-m">
+            <Image height={300} width={400} src={image} alt={description} />
+            <div className="project-text sm-mt sm-mb xxl-ml xxl-mr">
+                <h1 className="lg-f lg-fw">{name}</h1>
+                <p className="md-f md-fw">{description}</p>
+                <p className="tech-used md-f md-fw lg-mt lg-mb">{technologies}</p>
             </div>
-        </>
+            <a href={link}>
+                <button id="cta" className="dashed transition">Go to project</button>
+            </a>
+        </div>
     )
 }
